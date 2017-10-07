908829d93add1c0ccb8da9f1c4b2eb454fec7537
10shibu <shibu@brandeis.edu>
2017-10-07 00:20:20 -0400

    first push

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
   has_many :registrations
-  validates :zipcode, length: {ls: 5},   format: {with: / \d\d\d\d\d( \d\d\d)? / }
+  #validates :zipcode, length: {ls: 5},   format: {with: / \d\d\d\d\d( \d\d\d)? / }
   #has_many :events through: :registrations
 end
